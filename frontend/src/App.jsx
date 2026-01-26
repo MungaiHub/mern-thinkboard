f5b712a22b3e52049d2983a4295b98ce5fec5afd
MungaiHub <amosmungai085@gmail.com>
2026-01-26 15:05:00 +0300

    feat: enhance frontend routing and configuration with new pages and server settings

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,19 @@
 import React from 'react'
+import HomePage from './pages/HomaPage'
+import CreatePage from './pages/CreatePage'
+import NoteDetailPage from './pages/NoteDetailPage'
+import { Routes, Route } from "react-router-dom";
+import toast from "react-hot-toast";
 
 const App = () => {
   return (
     <div>
-      
+      <button className="btn btn-primary">click me</button>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/create" element={<CreatePage />} />
+        <Route path="/note/:id" element={<NoteDetailPage />} />
+      </Routes>
     </div>
   )
 }
