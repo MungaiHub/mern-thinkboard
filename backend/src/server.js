9b2ac83753e713ec0142a7eb5a5ae56317f7d1de
MungaiHub <amosmungai085@gmail.com>
2026-02-01 12:16:52 +0300

    fix: improve error handling in HomaPage and refine rate limiter middleware for enhanced response accuracy

--- backend/src/server.js
+++ backend/src/server.js
@@ -13,12 +13,14 @@ const app = express();
 
  
 //middleware
-app. use(express.json());
-app.use(rateLimiter);
-app.use("/api/notes", notesRoutes);
+
 app.use(cors({
   origin: "http://localhost:5173",
 }));
+app. use(express.json());
+app.use(rateLimiter);
+app.use("/api/notes", notesRoutes);
+
 
 connectDB().then(() => {
   app.listen(5001, () => {
